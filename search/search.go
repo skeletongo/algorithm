15b0460606f025e5c475cde0bce1dbb71de3aa0c
skeletongo <liuzhengr@qq.com>
2021-07-14 23:58:09 +0800

    add 二分搜索

--- search/search.go
+++ search/search.go
@@ -2,12 +2,14 @@ package search
 
 // Search 二分查找
 // v 查找的值
-// 返回 v 在数组 arr 中的索引，没有找到返回-1
+// 返回 v 在数组 arr 中的任意一个索引，没有找到返回-1
 func Search(arr []float64, v float64) int {
 	l := 0
 	r := len(arr) - 1
 
+	// 在arr[l...r]之中查找目标值
 	for l <= r {
+		// mid = (l + r)/2 防止极端情况下的整形溢出，使用下面的形式
 		mid := l + (r-l)/2
 		if v < arr[mid] {
 			r = mid - 1
@@ -20,10 +22,10 @@ func Search(arr []float64, v float64) int {
 	return -1
 }
 
-// Search2 二分查找(递归)
+// SearchR 二分查找(递归)
 // v 查找的值
-// 返回 v 在数组 arr 中的索引，没有找到返回-1
-func Search2(arr []float64, v float64) int {
+// 返回 v 在数组 arr 中的任意一个索引，没有找到返回-1
+func SearchR(arr []float64, v float64) int {
 	l := 0
 	r := len(arr) - 1
 	return searchR(arr, l, r, v)
@@ -43,42 +45,54 @@ func searchR(arr []float64, l, r int, v float64) int {
 	return mid
 }
 
-// Floor 在arr中搜索小于等于v的最大值所在的索引，没有找到返回-1
+// Floor 在数组arr中搜索值等于v的元素的索引，如果有多个，返回第一个
+// 如果没有则返回小于v的最大值元素的索引，如果有多个，返回最大索引，如果还没有就返回-1
 func Floor(arr []float64, v float64) int {
-	l := 0
+	l := -1
 	r := len(arr) - 1
 
-	ret := -1
-	for l <= r {
-		mid := l + (r-l)/2
-		if v < arr[mid] {
+	for l < r {
+		//
+		mid := l + (r-l+1)/2
+		if arr[mid] >= v {
 			r = mid - 1
-		} else if v > arr[mid] {
-			ret = mid
-			l = mid + 1
 		} else {
-			return mid
+			l = mid
 		}
 	}
-	return ret
+
+	// 如果该索引+1就是搜索值本身, 该索引+1即为返回值
+	if l+1 < len(arr) && arr[l+1] == v {
+		return l + 1
+	}
+
+	// 否则, 该索引即为返回值
+	return l
 }
 
-// Ceil 在arr中搜索大于等于v的最小值所在的索引，没有找到返回-1
+// Ceil 在数组arr中搜索值等于v的元素的索引，如果有多个，返回最后一个
+// 如果没有则返回大于v的最小值元素的索引，如果有多个，返回最小索引，如果还没有就返回-1
 func Ceil(arr []float64, v float64) int {
 	l := 0
-	r := len(arr) - 1
+	r := len(arr)
 
-	ret := -1
-	for l <= r {
+	for l < r {
+		//
 		mid := l + (r-l)/2
-		if v < arr[mid] {
-			ret = mid
-			r = mid - 1
-		} else if v > arr[mid] {
+		if arr[mid] <= v {
 			l = mid + 1
 		} else {
-			return mid
+			r = mid
 		}
 	}
-	return ret
+
+	// 如果该索引-1就是搜索值本身, 该索引-1即为返回值
+	if r-1 >= 0 && arr[r-1] == v {
+		return r - 1
+	}
+
+	if len(arr) == r {
+		return -1
+	}
+	return r
 }
--- search/search_test.go
+++ search/search_test.go
@@ -3,6 +3,7 @@ package search
 import (
 	"fmt"
 	"math/rand"
+	"sort"
 	"testing"
 	"time"
 )
@@ -12,132 +13,174 @@ func init() {
 }
 
 func TestSearch(t *testing.T) {
-	n := 10000
-	var arr []float64
-	for i := 0; i < 10; i++ {
-		arr = append(arr, float64(i))
-	}
-
-	for i := 0; i < n; i++ {
-		v := float64(rand.Intn(n))
-		if rand.Intn(2) == 0 {
-			v = -v
-		}
-		if rand.Intn(2) == 0 {
-			v += rand.Float64()
-		} else {
-			v -= rand.Float64()
-		}
-		s := Search(arr, v)
-
-		//fmt.Println(v,s)
-
-		if s == -1 {
-			if v >= 0 && v < 10 && float64(int(v)) == v {
-				fmt.Println("Search error", s, v)
+	for i := 0; i < 10000; i++ {
+		var arr []float64
+		for j := 0; j < 10; j++ {
+			v := float64(rand.Intn(10))
+			if rand.Intn(2) == 0 {
+				v = -v
 			}
-		} else {
-			if arr[s] != v {
-				fmt.Println("Search error", s, v)
+			arr = append(arr, v)
+		}
+
+		sort.Float64s(arr)
+
+		for j := 0; j < 10; j++ {
+			v := float64(rand.Intn(10))
+			if rand.Intn(2) == 0 {
+				v = -v
+			}
+
+			s := Search(arr, v)
+
+			if s == -1 {
+				for _, n := range arr {
+					if n == v {
+						t.Fatal("Search error")
+					}
+				}
+			} else {
+				if arr[s] != v {
+					fmt.Println("Search error", s, v)
+				}
 			}
 		}
 	}
 }
 
-func TestSearch2(t *testing.T) {
-	n := 10000
-	var arr []float64
-	for i := 0; i < 10; i++ {
-		arr = append(arr, float64(i))
-	}
-
-	for i := 0; i < n; i++ {
-		v := float64(rand.Intn(n))
-		if rand.Intn(2) == 0 {
-			v = -v
-		}
-		if rand.Intn(2) == 0 {
-			v += rand.Float64()
-		} else {
-			v -= rand.Float64()
-		}
-		s := Search2(arr, v)
-
-		//fmt.Println(v,s)
-
-		if s == -1 {
-			if v >= 0 && v < 10 && float64(int(v)) == v {
-				fmt.Println("Search error", s, v)
+func TestSearchR(t *testing.T) {
+	for i := 0; i < 10000; i++ {
+		var arr []float64
+		for j := 0; j < 10; j++ {
+			v := float64(rand.Intn(10))
+			if rand.Intn(2) == 0 {
+				v = -v
 			}
-		} else {
-			if arr[s] != v {
-				fmt.Println("Search error", s, v)
+			arr = append(arr, v)
+		}
+
+		sort.Float64s(arr)
+
+		for j := 0; j < 10; j++ {
+			v := float64(rand.Intn(10))
+			if rand.Intn(2) == 0 {
+				v = -v
+			}
+
+			s := SearchR(arr, v)
+
+			if s == -1 {
+				for _, n := range arr {
+					if n == v {
+						t.Fatal("SearchR error")
+					}
+				}
+			} else {
+				if arr[s] != v {
+					fmt.Println("SearchR error", s, v)
+				}
 			}
 		}
 	}
 }
 
 func TestFloor(t *testing.T) {
-	n := 10000
-	var arr []float64
-	for i := 0; i < 10; i++ {
-		arr = append(arr, float64(i))
-	}
-
-	for i := 0; i < n; i++ {
-		v := float64(rand.Intn(n))
-		if rand.Intn(2) == 0 {
-			v = -v
-		}
-		if rand.Intn(2) == 0 {
-			v += rand.Float64()
-		} else {
-			v -= rand.Float64()
-		}
-		s := Floor(arr, v)
-
-		//fmt.Println(v,s)
-
-		if s == -1 {
-			if v >= 0 {
-				fmt.Println("Floor error", s, v)
+	for i := 0; i < 10000; i++ {
+		var arr []float64
+		for j := 0; j < 10; j++ {
+			v := float64(rand.Intn(10))
+			if rand.Intn(2) == 0 {
+				v = -v
 			}
-		} else {
-			if arr[s] > v {
-				fmt.Println("Floor error", s, v)
+			switch rand.Intn(3) {
+			case 0:
+				v += rand.Float64()
+			case 1:
+				v -= rand.Float64()
+			default:
+			}
+			arr = append(arr, v)
+		}
+
+		sort.Float64s(arr)
+
+		for j := 0; j < 10; j++ {
+			v := float64(rand.Intn(10))
+			if rand.Intn(2) == 0 {
+				v = -v
+			}
+			switch rand.Intn(3) {
+			case 0:
+				v += rand.Float64()
+			case 1:
+				v -= rand.Float64()
+			default:
+			}
+
+			s := Floor(arr, v)
+
+			if s == -1 {
+				if v >= arr[0] {
+					t.Fatal("Floor error -1")
+				}
+			} else {
+				if arr[s] == v && s-1 > 0 && arr[s-1] == v {
+					fmt.Println("Floor error =", s, v)
+				}
+				if arr[s] != v && (arr[s] > v || (s+1 < len(arr) && arr[s+1] < v)) {
+					fmt.Println("Floor error !=", s, v)
+				}
 			}
 		}
 	}
 }
 
 func TestCeil(t *testing.T) {
-	n := 10000
-	var arr []float64
-	for i := 0; i < 10; i++ {
-		arr = append(arr, float64(i))
-	}
-
-	for i := 0; i < n; i++ {
-		v := float64(rand.Intn(n))
-		if rand.Intn(2) == 0 {
-			v = -v
-		}
-		if rand.Intn(2) == 0 {
-			v += rand.Float64()
-		} else {
-			v -= rand.Float64()
-		}
-		s := Ceil(arr, v)
-
-		//fmt.Println(v,s)
-
-		if s == -1 {
-			if v < 9 {
-				fmt.Println("Ceil error", s, v)
+	for i := 0; i < 10000; i++ {
+		var arr []float64
+		for j := 0; j < 10; j++ {
+			v := float64(rand.Intn(10))
+			if rand.Intn(2) == 0 {
+				v = -v
 			}
-		} else {
-			if arr[s] < v {
-				fmt.Println("Ceil error", s, v)
+			switch rand.Intn(3) {
+			case 0:
+				v += rand.Float64()
+			case 1:
+				v -= rand.Float64()
+			default:
+			}
+			arr = append(arr, v)
+		}
+
+		sort.Float64s(arr)
+
+		for j := 0; j < 10; j++ {
+			v := float64(rand.Intn(10))
+			if rand.Intn(2) == 0 {
+				v = -v
+			}
+			switch rand.Intn(3) {
+			case 0:
+				v += rand.Float64()
+			case 1:
+				v -= rand.Float64()
+			default:
+			}
+
+			s := Ceil(arr, v)
+
+			if s == -1 {
+				if v <= arr[0] {
+					t.Fatal("Ceil error -1")
+				}
+			} else {
+				if arr[s] == v && s+1 < len(arr) && arr[s+1] == v {
+					fmt.Println("Ceil error =", s, v)
+				}
+				if arr[s] != v && (arr[s] < v || (s-1 > 0 && arr[s-1] > v)) {
+					fmt.Println("Ceil error !=", s, v)
+				}
 			}
 		}
 	}
